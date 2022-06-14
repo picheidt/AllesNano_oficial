78b300e75f58994b1a9bf2d3018a0da6a346c205
picheidt <viniciuspicheidt@gmail.com>
2022-06-13 21:42:52 -0300

    envia_email_inicial e test do gitignore

---
+++ env/env-example.php
@@ -0,0 +1,4 @@
+<?php
+$PASS = '[coloca a senha aqui]'
+
+?>
--- script/mail.php
+++ script/mail.php
@@ -1,11 +1,33 @@
 <?php
-    // envia email
-    // header
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+include '../env/env.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 
-    // informações do emissor
+try {
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = 'allesnanotest@gmail.com';
+    $mail->Password = $PASS;
+    $mail->Port = 587;
 
-    // mensagem
+    $mail->setFrom('remetente@email.com.br');
+    $mail->addAddress('allesnanotest@gmail.com', 'Allesnano');
 
+    $mail->isHTML(true);
+    $mail->Subject = 'Assunto do email';
+    $mail->Body    = 'Este é o conteúdo da mensagem em <b>HTML!</b>';
+
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 ?>
